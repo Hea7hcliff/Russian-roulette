1a5ef338a4c6980d2844dbc8525919d5114eaf60
JAMK_04 <JAMK_04@FIL658509.bit.bittium.com>
2018-03-16 12:28:45 +0200

    v1.0

--- russian_roulette.py
+++ russian_roulette.py
@@ -2,40 +2,50 @@ import random
 
 def main():
     winnings = 0
-    
-    rounds = input("How many rounds? ")
-    cylinder = [1] * int(rounds)
-    while len(cylinder) < 6:
-        cylinder.append(0)
-    print(cylinder)
+    alive = True
 
-    rand = random.randint(1,6)
-    print("rand:", rand)
+    while(alive):
+        print("Winnings:", winnings)
+        bet = input("Set a bet? ")
+        rounds = input("How many rounds will be set in cylinder? ")
+
+        cylinder = [1] * int(rounds)
+
+        while len(cylinder) < 6:
+            cylinder.append(0)
+        
+        cylinder = spin(cylinder)
+
+        if cylinder[0] == 1:
+            alive = False
+            print("You dead")
+        else:
+            profit = multiplier(bet, rounds)
+            winnings += profit
+            print("You won", profit)
+
+        print("###########################")
+           
+def spin(cylinder):
+    print("Cylinder before spin:", cylinder)
+    
     temp = [0] * 6
-    print(temp)
+    
+    steps = random.randint(1,6)
+    print("Indexes forward:", steps)
+    print("Total cycles:",round(steps/6,2))
+        
     for index, value in enumerate(cylinder):
-        new_index = index + rand
-        print(new_index)
+        new_index = index + steps
         if new_index > 5:
-             new_index = new_index % 6
-             print(new_index)
+            new_index = new_index % 6
         temp[new_index] = value
     cylinder = temp[:]
-
-    if cylinder[0] == 1:
-        print("You dead")
-    else:
-        value = multiplier(bet, rounds)
-        winnings += value
-        print(f"You won {value}")
-        
-       
-        
+    print("Cylinder after spin:", cylinder)
+    return cylinder
     
-
 def multiplier(bet, rounds):
-    value = bet * rounds * 1.2
-    return value
+    profit = int(bet) * int(rounds) * 1.2
+    return profit
     
-
 main()
